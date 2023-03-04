8a81fb54f9a37067e203b7031bb5ef281cae7e43
Arsad <ayush.aak1998@gmail.com>
2023-03-04 16:10:48 +0530

    first commit

---
+++ src/test/java/com/iris/HealthCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
