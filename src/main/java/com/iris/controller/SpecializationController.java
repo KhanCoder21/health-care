3a269824312748c65de53f64d7757cc549ea69e3
Arsad <ayush.aak1998@gmail.com>
2023-03-05 20:15:14 +0530

    code for register page

--- src/main/java/com/iris/controller/SpecializationController.java
+++ src/main/java/com/iris/controller/SpecializationController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.iris.entity.Specialization;
@@ -19,7 +21,29 @@ public class SpecializationController {
 	private SpecializationService service;
 
 	/**
-	 * 1. display all Specialization
+	 * 1. show register page
+	 */
+	@GetMapping("/register")
+	public String displayRegister() {
+		return "specialization-register";
+	}
+
+	/**
+	 *  2. On submit from save data
+	 * @param specialization
+	 * @param model
+	 * @return
+	 */
+	@PostMapping("/save")
+	public String saveRegisterForm(@ModelAttribute Specialization specialization, Model model) {
+		Long specId = service.saveSpecialization(specialization);
+		String message= "Record "+specId+" is created successfully";
+		model.addAttribute("message", message);
+		return "specialization-register";
+	}
+
+	/**
+	 * 3. display all SpeciaSpecializationlization
 	 * 
 	 * @param model
 	 * @return
@@ -27,7 +51,7 @@ public class SpecializationController {
 	@GetMapping("/all")
 	public String viewAllSpecialization(Model model) {
 		List<Specialization> allSpecialization = service.getAllSpecialization();
-		System.out.println("Printing allSpecialization: "+allSpecialization);
+		System.out.println("Printing allSpecialization: " + allSpecialization);
 		model.addAttribute("allSpecialization", allSpecialization);
 		return "specialization-data";
 	}
